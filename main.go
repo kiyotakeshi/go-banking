2d3b7975f7b5eeeef1e4d27e23996148abc8d1c7
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2022-02-24 14:57:02 +0900

    add customer endpoint(return json)

--- main.go
+++ main.go
@@ -1,17 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+type Customer struct {
+	Name    string `json:"name"`
+	City    string `json:"city"`
+	Zipcode string `json:"zip_code"`
+}
+
 func main() {
+
 	// define routes
 	http.HandleFunc("/greeting", greet)
+	http.HandleFunc("/customers", greetCustomers)
 
 	http.ListenAndServe("localhost:8000", nil)
 }
 
 func greet(w http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(w, "hello world")
+}
+
+func greetCustomers(w http.ResponseWriter, request *http.Request) {
+	customers := []Customer{
+		{"mike", "tokyo", "103-0027"},
+		{"popcorn", "tokyo", "104-0061"},
+	}
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(customers)
 }

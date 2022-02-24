37c5c6d8056989f81aa4a4dbe41df0d89376e48c
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2022-02-24 15:12:24 +0900

    read request header and return xml response

--- main.go
+++ main.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"net/http"
 )
 
 type Customer struct {
-	Name    string `json:"name"`
-	City    string `json:"city"`
-	Zipcode string `json:"zip_code"`
+	Name    string `json:"name" xml:"name"`
+	City    string `json:"city" xml:"city"`
+	Zipcode string `json:"zip_code" xml:"zip_code"`
 }
 
 func main() {
-
 	// define routes
 	http.HandleFunc("/greeting", greet)
 	http.HandleFunc("/customers", greetCustomers)
@@ -25,11 +25,17 @@ func greet(w http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(w, "hello world")
 }
 
-func greetCustomers(w http.ResponseWriter, request *http.Request) {
+func greetCustomers(w http.ResponseWriter, r *http.Request) {
 	customers := []Customer{
 		{"mike", "tokyo", "103-0027"},
 		{"popcorn", "tokyo", "104-0061"},
 	}
-	w.Header().Add("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(customers)
+
+	if r.Header.Get("Content-Type") == "application/xml" {
+		w.Header().Add("Content-Type", "application/xml")
+		xml.NewEncoder(w).Encode(customers)
+	} else {
+		w.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(customers)
+	}
 }
